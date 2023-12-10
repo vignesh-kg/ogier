370df40e34105a73c227fc0458471731fc94fb24
vignesh-kg <vigneshkg515@gmail.com>
2023-12-10 14:38:37 +0200

    Read config from main-config.yaml

--- src/main/java/org/ogier/msbuilder/config/BeanConfig.java
+++ src/main/java/org/ogier/msbuilder/config/BeanConfig.java
@@ -2,10 +2,13 @@ package org.ogier.msbuilder.config;
 
 import org.ogier.msbuilder.util.implementation.DirectoryUtil;
 import org.ogier.msbuilder.util.interfaces.IDirectoryUtil;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = {"classpath:main-config.yaml"}, factory = YamlPropertyLoaderFactory.class)
 public class BeanConfig {
 
     @Bean
--- src/main/java/org/ogier/msbuilder/main/OgierBuilder.java
+++ src/main/java/org/ogier/msbuilder/main/OgierBuilder.java
@@ -4,6 +4,7 @@ import org.ogier.msbuilder.util.interfaces.IDirectoryUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,15 +12,15 @@ public class OgierBuilder
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(OgierBuilder.class);
 
-    //@Value("${org.ogier.modules}")
-    //private String listOfModules;
+    @Value("${org.ogier.modules}")
+    private String listOfModules;
 
     @Autowired
     private IDirectoryUtil directoryUtil;
 
     public void build(String[] args)
     {
-        //LOGGER.info(listOfModules);
+        LOGGER.info(listOfModules);
         directoryUtil.createDirectory();
     }
 }
