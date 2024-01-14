17dca24a8b3833377e302f69f4145e8af50e72d6
vignesh-kg <vigneshkg515@gmail.com>
2024-01-14 17:29:06 +0200

    Added mandatory argument check

--- src/main/java/org/ogier/msbuilder/main/OgierBuilder.java
+++ src/main/java/org/ogier/msbuilder/main/OgierBuilder.java
@@ -6,8 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class OgierBuilder {
@@ -15,12 +14,11 @@ public class OgierBuilder {
     @Autowired
     private IDirectoryUtil directoryUtil;
 
-    public void build(String[] args) {
+    public void build(String[] args) throws Exception {
         directoryUtil.createModules(createArgsMap(args));
     }
 
-    private Map<String, String> createArgsMap(String[] args)
-    {
+    private Map<String, String> createArgsMap(String[] args) throws Exception {
         Map<String, String> argsMap = new HashMap<>();
 
         for(String arg : args)
@@ -28,7 +26,20 @@ public class OgierBuilder {
             String[] argSplited = arg.split("=");
             argsMap.put(argSplited[0].toLowerCase(),argSplited[1]);
         }
-
+        validateArgs(argsMap.keySet());
         return argsMap;
+    }
+
+    private void validateArgs(Set<String> args) throws Exception {
+        //msName=ogier directoryPath="/home/vigz/workspace/OS project/" groupId=org.ogier.ms
+        List<String> mandatoryArgs = new ArrayList<>(Arrays.asList("msName", "directoryPath", "groupId"));
+
+        for(String mandatoryArg : mandatoryArgs)
+        {
+            if(!args.contains(mandatoryArg))
+            {
+                throw new Exception("Missing Mandatory argument " + mandatoryArg);
+            }
+        }
     }
 }
